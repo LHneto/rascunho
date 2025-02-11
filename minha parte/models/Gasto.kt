aa7c7c1b152858031fd7c5dc71a7f8336f3ec6ba
Luiz Haroldo <61132305+LHneto@users.noreply.github.com>
2025-02-10 23:52:20 -0300

    Delete minha parte directory

--- minha parte/models/Gasto.kt
+++
@@ -1,9 +0,0 @@
-package com.example.meubolso.models
-
-data class Gasto(
-    val id: Int,
-    val nome: String,
-    val valor: Double,
-    val categoria: String,
-    val data: String // Formato dd/MM/yyyy
-)
--- minha parte/models/Meta.kt
+++
@@ -1,9 +0,0 @@
-package com.example.meubolso.models
-
-data class Meta(
-    val id: Int,
-    val nome: String,
-    val valor: Double,
-    val dataConclusao: String, // Formato dd/MM/yyyy
-    val descricao: String
-)
